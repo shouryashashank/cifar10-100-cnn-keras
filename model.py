d0342714bd8bb4bfad7a8f544fe5d83fa557dd63
Shourya Shashank <shouryashashank@gmail.com>
2019-01-22 21:48:34 +0530

    created model.py

---
+++ model.py
@@ -0,0 +1,47 @@
+def createModel():
+    model = Sequential()
+    # The first two layers with 32 filters of window size 3x3
+    model.add(ZeroPadding2D(4, input_shape=train_data.shape[1:]))
+    
+    #layer1
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu',))
+    model.add(Conv2D(64, (3, 3), padding='same', activation='relu',))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    
+    #layer2
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    #layer3
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    
+    #layer4
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+    model.add(Dropout(0.25))
+
+    #layer5
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(nClasses, activation='softmax'))
+    
+    return model
+  
+m=createModel()
+
+m.summary()
